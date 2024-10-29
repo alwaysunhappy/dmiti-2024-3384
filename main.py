dad12a14d7621519c5bc39758867de7e62fec487
alwaysunhappy <lizakot2018@mail.ru>
2024-10-29 22:42:45 +0300

    add ADD_NN_N, ABS_Z_N, DEG_P_N

--- main.py
+++ main.py
@@ -26,8 +26,44 @@ class Natural:
                     return 1
         return 0
 
+    def addition_of_natural(self):
+        """
+        ADD_NN_N
+        Складывает два натурльынх числа и возвращает результат
+        """
+        shift = 0  # перенос в следущий разряд
+        if self.cmp_of_natural_number() == 2 or self.cmp_of_natural_number() == 0:  # если первое число больше или равно
+            answer = [0] * (self.n_first + 1)  # загатавливаем под ответ массив длиной на один разряд больше
+            for i in range(1, self.n_first + 2):  # перебираем так, чтобы идти с конца числа -1 индекс, -2 индекс и т.д.
+                if self.n_second >= i:  # если у второго числа еще есть, что складывать
+                    total = self.values_first[-i] + self.values_second[-i] + shift  # складываем две цифры и перенос
+                    answer[-i] = total % 10  # получаем цифру которая запишется в этом разряде
+                    shift = total // 10  # считаем перенос на след разряд
+                elif self.n_first >= i:  # когда у второго числа уже нечего складывать
+                    total = self.values_first[-i] + shift
+                    answer[-i] = total % 10
+                    shift = total // 10
+                else:
+                    answer[-i] = shift  # обрабатываем случай, если перенос случился на первом разрде (9 + 99 = 18)
+                    # <- здесь запишем единицу
+        else:
+            answer = [0] * (self.n_second + 1)
+            for i in range(1, self.n_second + 2):
+                if self.n_first >= i:
+                    total = self.values_first[-i] + self.values_second[-i] + shift
+                    answer[-i] = total % 10
+                    shift = total // 10
+                elif self.n_second >= i:
+                    total = self.values_second[-i] + shift
+                    answer[-i] = total % 10
+                    shift = total // 10
+                else:
+                    answer[-i] = shift
+        answer = list(map(str, answer))
+        return int("".join(answer)) if answer[0] != 0 else int("".join(answer[1:]))
 
-# (n; values[..], sign) - знак числа (1 — минус, 0 — плюс) номер старшей позиции и массив цифр
+
+# (n; values[..], sign) - [[длины чисел], [сами числа в виде массивов(каждое число отдельный массив)], [знак числа]]
 class Integers:
     def __init__(self, n: list[int], values: list[list], sign: list[bool]):
         self.sign_first = sign[0]  # знак первого числа, если True, то минус
@@ -37,6 +73,18 @@ class Integers:
             self.sign_second = sign[1]  # знак второго числа
             self.n_second = n[1]  # длина второго массива
             self.values_second = values[1]  # второе целое число
+
+    def abs_integer(self):
+        """
+
+            ABS_Z_N
+            Функция берет модуль числа и возвращает его
+
+        """
+        values = list(map(str, self.values_first))  # делаем массив числа строковым типом
+        if self.sign_first is True:  # проверяем знак
+            self.sign_first = False  # если знак числа минус, то меняем знак на положительный
+        return int(''.join(values))  # выводим получившиеся число
 
 
 # массив пар (целое; натуральное), первое имеет смысл числителя, второе - знаменателя
@@ -59,6 +107,14 @@ class Polynomial:
             self.coefficients_second = array_polynomial[1][1]  # коэффиценты второго многочлена
             print(self.coefficients_second, self.degree_second)
 
+    def degree_polynomial(self):
+        """
+
+            Функция возвращает степень многочлена
+
+        """
+        return self.degree_first  # возвращает степень многочлена
+
 
 def input_natural():
     print("Введите натуральое число:", end=' ')
@@ -77,7 +133,7 @@ def create_natural(quantity: int):
 
 
 def input_integer():
-    sign = bool(input("Введите знак числа (1 - отрицательное, 0 положительное)"))
+    sign = bool(input("Введите знак числа (1 - отрицательное, 0 положительное): "))
     print("Введите целое число:", end=' ')
     array = [int(i) for i in input()]
     return array, sign
@@ -129,7 +185,7 @@ def create_polynomial(quantity: int):
             new_coefficients[index] = coefficients[i]
         array_coef.append(new_coefficients)
     if quantity == 1:
-        Polynomial([[len(array_coef[0]) - 1, array_coef[0]]])
+        return Polynomial([[len(array_coef[0]) - 1, array_coef[0]]])
     else:
         return Polynomial([[len(array_coef[0]) - 1, array_coef[0]], [len(array_coef[1]) - 1, array_coef[1]]])
 
@@ -142,7 +198,16 @@ class Launch:
         if self.number == 1:
             natural = create_natural(2)
             print(natural.cmp_of_natural_number())
+        if self.number == 4:
+            natural = create_natural(2)
+            print(natural.addition_of_natural())
+        if self.number == 15:
+            integers = create_integer(1)
+            print(integers.abs_integer())
+        if self.number == 38:
+            polynomial = create_polynomial(1)
+            print(polynomial.degree_polynomial())
 
 
 if __name__ == "__main__":
-    Launch(1).start_function()
+    Launch(...).start_function()
