de6acaa2c04291e3594a542fc079667a7701b681
alwaysunhappy <lizakot2018@mail.ru>
2024-11-01 23:38:04 +0300

    fix

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ class Natural:
         """
         return Natural(self.n, self.values.copy())
 
-    def __str__(self):
-        return "".join(list(map(str, self.values)))
-
 
 class Integers(Natural):
     """
@@ -46,10 +43,6 @@ class Integers(Natural):
     def __init__(self, n: int, values: list[int], sign: bool):
         super().__init__(n, values)
         self.sign = sign  # знак числа, если True, то минус
-        
-    def __str__(self):
-        sign = "- " if self.sign else ""
-        return sign + "".join(list(map(str, self.values)))
 
 
 class Rational:
@@ -67,9 +60,6 @@ class Rational:
         self.numerator = array_rational[0]  # числитель дроби
         self.denominator = array_rational[1]  # знаменатель дроби
 
-    def __str__(self):
-        return self.numerator.__str__() + "/" + self.denominator.__str__()
-
 
 class Polynomial:
     """
@@ -86,118 +76,3 @@ class Polynomial:
     def __init__(self, array_polynomial: list[Natural, list[Rational]]):
         self.degree = array_polynomial[0]  # степень многочлена
         self.coefficients = array_polynomial[1]  # коэффиценты многочлена
-
-    def __str__(self):
-        result = ''
-        for i in range(len(self.coefficients) - 1, - 1, -1):
-            if self.coefficients[i].numerator.values != [0]:
-                result += '+ ' if self.coefficients[i].numerator.sign is False else ''
-                result += self.coefficients[i].__str__()
-                if i != 0:
-                    result += 'x^' + str(i)
-                result += ' '
-        if result[0] == '+':
-            result = result[1:]
-        return result
-
-        
-def input_natural():
-    print("Введите натуральое число:", end=' ')
-    array = [int(i) for i in input()]
-    return create_natural(array)
-
-
-def create_natural(array):
-    return Natural(len(array), array)
-
-
-def input_integer():
-    print("Введите целое число:", end=' ')
-    array = input()
-    if array[0] == '-':
-        sign = True
-        array = array[1:]
-    else:
-        sign = False
-    array = [int(i) for i in array]
-    return create_integer(array, sign)
-
-
-def create_integer(array, sign):
-    return Integers(len(array), array, sign)
-
-
-def input_rational():
-    print("Введите дробь в формате x/y, где числитель - целое, знаменатель натуралньое.")
-    numerator, denominator = input().split('/')
-    if numerator[0] == '-':
-        sign = True
-        numerator = numerator[1:]
-    else:
-        sign = False
-    numerator = [int(i) for i in numerator]
-    denominator = [int(i) for i in denominator]
-    return create_rational(create_integer(numerator, sign), create_natural(denominator))
-
-
-def create_rational(numerator, denominator):
-    return Rational([numerator, denominator])
-
-
-def input_polynomial():
-    coefficients = []
-    degrees = []
-    max_degree = 0
-    while True:
-        term = input("Введите член многочлена вида 3/4x^2(или 'стоп' для завершения): ")
-        if term.lower() != 'стоп':
-            parts = term.split('x^')
-            degree = parts[1]
-            max_degree = max(int(degree), max_degree)
-            numerator, denominator = parts[0].split('/')
-            if numerator[0] == '-':
-                sign = True
-                numerator = numerator[1:]
-            else:
-                sign = False
-            numerator = [int(i) for i in numerator]
-            denominator = [int(i) for i in denominator]
-            degree = [int(i) for i in degree]
-            coefficients.append(create_rational(create_integer(numerator, sign), create_natural(denominator)))
-            degrees.append(create_natural(degree))
-        else:
-            break
-    return create_polynomial(coefficients, degrees, max_degree)
-
-
-def create_polynomial(coefficients, degrees, max_degree):
-    new_degrees = []
-    for degree in range(max_degree + 1):
-        new_degrees.append(degree)
-    new_coefficients = [Rational([Integers(1, [0], False), Natural(1, [1])])] * len(new_degrees)
-    for i, degree in enumerate(degrees):
-        index = 0
-        for k in range(len(new_degrees)):
-            if int("".join(list(map(str, degree.values)))) == new_degrees[k]:
-                index = k
-                break
-        new_coefficients[index] = coefficients[i]
-    max_degree = [int(i) for i in str(max_degree)]
-    return Polynomial([Natural(len(max_degree), max_degree), new_coefficients])
-
-
-class Launch:
-    def __init__(self, number):
-        self.number = number
-
-    def start_function(self):
-        if self.number == 1:
-            natural = input_natural()
-            natural_second = input_natural()
-            print(natural.cmp_of_natural_number(natural_second))
-
-
-
-if __name__ == "__main__":
-    a = int(input("Введите номер функции: "))
-    Launch(a).start_function()
