44393b58918a55c0f298096a2eaf18f2be5a6382
alwaysunhappy <lizakot2018@mail.ru>
2024-11-01 23:44:20 +0300

    add input/output/launch

--- main.py
+++ main.py
@@ -27,24 +27,8 @@ class Natural:
         """
         return Natural(self.n, self.values.copy())
 
-    def cmp_of_natural_number(self, other):
-        """
-            Сравнивает два натуральных числа и возвращает:
-            2 - если первое число больше второго,
-            0 - если числа равны,
-            1 - если первое число меньше второго.
-        """
-        if self.n > other.n:  # проверяем, больше ли длина первого числа длины второго, что означает, что первое больше
-            return 2
-        elif self.n < other.n:  # иначе, больше второе число
-            return 1
-        else:  # если их длины равны, проверяем числа, начиная с нулевого разряда
-            for i in range(self.n):
-                if self.values[i] > other.values[i]:
-                    return 2
-                elif self.values[i] < other.values[i]:
-                    return 1
-        return 0
+    def __str__(self):
+        return "".join(list(map(str, self.values)))
 
 
 class Integers(Natural):
@@ -62,15 +46,10 @@ class Integers(Natural):
     def __init__(self, n: int, values: list[int], sign: bool):
         super().__init__(n, values)
         self.sign = sign  # знак числа, если True, то минус
-
-    def abs_integer(self):
-        """
-            ABS_Z_N
-            Функция берет модуль числа и возвращает его
-        """
-        if self.sign is True:  # проверяем знак
-            self.sign = False  # если знак числа минус, то меняем знак на положительный
-        return self  # выводим получившиеся число
+        
+    def __str__(self):
+        sign = "- " if self.sign else ""
+        return sign + "".join(list(map(str, self.values)))
 
 
 class Rational:
@@ -87,6 +66,9 @@ class Rational:
     def __init__(self, array_rational: list[Integers, Natural]):
         self.numerator = array_rational[0]  # числитель дроби
         self.denominator = array_rational[1]  # знаменатель дроби
+
+    def __str__(self):
+        return self.numerator.__str__() + "/" + self.denominator.__str__()
 
 
 class Polynomial:
@@ -105,9 +87,117 @@ class Polynomial:
         self.degree = array_polynomial[0]  # степень многочлена
         self.coefficients = array_polynomial[1]  # коэффиценты многочлена
 
-    def degree_polynomial(self):
-        """
-            DEG_P_N
-        Функция возвращает степень многочлена
-        """
-        return self.degree  # возвращает степень многочлена
+    def __str__(self):
+        result = ''
+        for i in range(len(self.coefficients) - 1, - 1, -1):
+            if self.coefficients[i].numerator.values != [0]:
+                result += '+ ' if self.coefficients[i].numerator.sign is False else ''
+                result += self.coefficients[i].__str__()
+                if i != 0:
+                    result += 'x^' + str(i)
+                result += ' '
+        if result[0] == '+':
+            result = result[1:]
+        return result
+
+        
+def input_natural():
+    print("Введите натуральое число:", end=' ')
+    array = [int(i) for i in input()]
+    return create_natural(array)
+
+
+def create_natural(array):
+    return Natural(len(array), array)
+
+
+def input_integer():
+    print("Введите целое число:", end=' ')
+    array = input()
+    if array[0] == '-':
+        sign = True
+        array = array[1:]
+    else:
+        sign = False
+    array = [int(i) for i in array]
+    return create_integer(array, sign)
+
+
+def create_integer(array, sign):
+    return Integers(len(array), array, sign)
+
+
+def input_rational():
+    print("Введите дробь в формате x/y, где числитель - целое, знаменатель натуралньое.")
+    numerator, denominator = input().split('/')
+    if numerator[0] == '-':
+        sign = True
+        numerator = numerator[1:]
+    else:
+        sign = False
+    numerator = [int(i) for i in numerator]
+    denominator = [int(i) for i in denominator]
+    return create_rational(create_integer(numerator, sign), create_natural(denominator))
+
+
+def create_rational(numerator, denominator):
+    return Rational([numerator, denominator])
+
+
+def input_polynomial():
+    coefficients = []
+    degrees = []
+    max_degree = 0
+    while True:
+        term = input("Введите член многочлена вида 3/4x^2(или 'стоп' для завершения): ")
+        if term.lower() != 'стоп':
+            parts = term.split('x^')
+            degree = parts[1]
+            max_degree = max(int(degree), max_degree)
+            numerator, denominator = parts[0].split('/')
+            if numerator[0] == '-':
+                sign = True
+                numerator = numerator[1:]
+            else:
+                sign = False
+            numerator = [int(i) for i in numerator]
+            denominator = [int(i) for i in denominator]
+            degree = [int(i) for i in degree]
+            coefficients.append(create_rational(create_integer(numerator, sign), create_natural(denominator)))
+            degrees.append(create_natural(degree))
+        else:
+            break
+    return create_polynomial(coefficients, degrees, max_degree)
+
+
+def create_polynomial(coefficients, degrees, max_degree):
+    new_degrees = []
+    for degree in range(max_degree + 1):
+        new_degrees.append(degree)
+    new_coefficients = [Rational([Integers(1, [0], False), Natural(1, [1])])] * len(new_degrees)
+    for i, degree in enumerate(degrees):
+        index = 0
+        for k in range(len(new_degrees)):
+            if int("".join(list(map(str, degree.values)))) == new_degrees[k]:
+                index = k
+                break
+        new_coefficients[index] = coefficients[i]
+    max_degree = [int(i) for i in str(max_degree)]
+    return Polynomial([Natural(len(max_degree), max_degree), new_coefficients])
+
+
+class Launch:
+    def __init__(self, number):
+        self.number = number
+
+    def start_function(self):
+        if self.number == 1:
+            natural = input_natural()
+            natural_second = input_natural()
+            print(natural.cmp_of_natural_number(natural_second))
+
+
+
+if __name__ == "__main__":
+    a = int(input("Введите номер функции: "))
+    Launch(a).start_function()
